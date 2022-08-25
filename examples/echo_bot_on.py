7e1bf189ee671b17568258ca3cc40a88cd44063b
evilbeast <houshao55@gmail.com>
2022-08-25 14:18:47 +0800

    修改echo_bot例子不回复群消息

--- examples/echo_bot_on.py
+++ examples/echo_bot_on.py
@@ -12,9 +12,10 @@ def on_recv_text_msg(wechat_instance: ntchat.WeChat, message):
     data = message["data"]
     from_wxid = data["from_wxid"]
     self_wxid = wechat_instance.get_login_info()["wxid"]
+    room_wxid = data["room_wxid"]
 
-    # 判断消息不是自己发的，并回复对方
-    if from_wxid != self_wxid:
+    # 判断消息不是自己发的并且不是群消息时，回复对方
+    if from_wxid != self_wxid and not room_wxid:
         wechat_instance.send_text(to_wxid=from_wxid, content=f"你发送的消息是: {data['msg']}")
 
 
