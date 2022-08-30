8fc25929268e451e057cd53beeceb0873c6b524f
evilbeast <houshao55@gmail.com>
2022-08-30 16:10:10 +0800

    新增cmd窗口关闭例子

---
+++ examples/close_log2.py
@@ -0,0 +1,33 @@
+import sys
+import time
+import logging
+logging.disable(logging.INFO)
+
+import ntchat
+
+wechat = ntchat.WeChat()
+
+# 打开pc微信, smart: 是否管理已经登录的微信
+wechat.open(smart=True)
+
+
+# 注册消息回调
+@wechat.msg_register(ntchat.MT_RECV_TEXT_MSG)
+def on_recv_text_msg(wechat_instance: ntchat.WeChat, message):
+    data = message["data"]
+    from_wxid = data["from_wxid"]
+    self_wxid = wechat_instance.get_login_info()["wxid"]
+    room_wxid = data["room_wxid"]
+
+    # 判断消息不是自己发的并且不是群消息时，回复对方
+    if from_wxid != self_wxid and not room_wxid:
+        wechat_instance.send_text(to_wxid=from_wxid, content=f"你发送的消息是: {data['msg']}")
+
+
+# 以下是为了让程序不结束，如果有用于PyQt等有主循环消息的框架，可以去除下面代码
+try:
+    while True:
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    ntchat.exit_()
+    sys.exit()
