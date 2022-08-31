8fc68a4bac19f25bc36d90496a2f5dd0185d9d89
evilbeast <houshao55@gmail.com>
2022-08-31 12:18:25 +0800

    去除文件log

--- ntchat/utils/logger.py
+++ ntchat/utils/logger.py
@@ -1,7 +1,5 @@
 import os
 import logging
-from datetime import datetime
-from .xdg import get_log_dir
 from .. import conf
 
 
@@ -18,24 +16,6 @@ def get_logger(name: str) -> logging.Logger:
     logger.handlers = []
     logger.setLevel(log_level)
     logger.propagate = False
-
-    # create file handler and set level to debug
-    if conf.LOG_FILE_KEY in os.environ:
-        filepath = os.environ[conf.LOG_FILE_KEY]
-    else:
-        base_dir = get_log_dir()
-        if not os.path.exists(base_dir):
-            os.mkdir(base_dir)
-
-        time_now = datetime.now()
-        time_format = '%Y-%m-%d-%H-%M'
-
-        filepath = f'{base_dir}/log-{time_now.strftime(time_format)}.txt'
-
-    file_handler = logging.FileHandler(filepath, 'a', encoding='utf-8')
-    file_handler.setLevel(log_level)
-    file_handler.setFormatter(log_formatter)
-    logger.addHandler(file_handler)
 
     # create console handler and set level to info
     console_handler = logging.StreamHandler()
